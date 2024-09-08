19bb82a0d48dbdbb3fd5fbe416343d1a5b6e85c0
Tsundere-Musume <maharjanelvis55@gmail.com>
2024-09-07 21:36:53 -0500

    added * and +
    added * and + for ascii displayable characters, still doesn't work for .* or .+'

--- regex/Regex.java
+++ regex/Regex.java
@@ -1,52 +1,115 @@
 import java.util.ArrayList;
 
-public class Regex{
+public class Regex {
     private String pattern;
-    // stored as a state transition table.
-    // each element of a list is a new state 
+    // INFO: stored as a state transition table.
+    // each element of a list is a new state
     // and the elements of the array inside it are the events.
     private ArrayList<int[]> compiledPattern;
-    
+
     public Regex(String pattern) {
         this.pattern = pattern;
         compiledPattern = this.compile();
     }
 
-    // should throw some kind of error if not valid
+    private boolean isValidAscii(char c) {
+        return c >= 32 && c <= 126;
+    }
+
+    private void setRange(int[] arr, int min, int max, int val) {
+        for (int i = min; i <= max; ++i) {
+            arr[i] = val;
+        }
+
+    }
+
+    // TODO: should throw some kind of error if not valid
     ArrayList<int[]> compile() {
         ArrayList<int[]> obj = new ArrayList<int[]>();
         obj.add(new int[128]);
-        for(int i = 0; i < pattern.length(); ++i){
-                char c = pattern.charAt(i);
-                if (c >= 65 && c <= 122){
-                    int[] state = new int[128];
+        for (int i = 0; i < pattern.length(); ++i) {
+            char c = pattern.charAt(i);
+            int[] state = new int[128];
+            char prev = i > 0 ? pattern.charAt(i - 1) : '\0';
+            switch (c) {
+                case '.':
+                    setRange(state, 32, 126, obj.size() + 1);
+                    break;
+
+                case '*':
+                case '+':
+                    if (i == '\0' || !isValidAscii(prev) || prev == '+' || prev == '.') {
+                        obj.clear();
+                        return obj;
+                    }
+                    if (c == '*') {
+                        setRange(obj.getLast(), 32, 126, obj.size());
+                    }
+                    if (i < pattern.length() - 1) {
+                        setRange(state, 32, 126, obj.size() + 1);
+                    } else {
+                        setRange(state, 32, 126, 0);
+                    }
+                    state[prev] = obj.size();
+                    break;
+
+                default:
+                    if (c < 65 || c > 122) {
+                        obj.clear();
+                        return obj;
+                    }
                     state[c] = obj.size() + 1;
-                    obj.add(state);
-                }else{
-                    obj.clear();
-                    return obj;
-                }
+                    break;
+            }
+            obj.add(state);
         }
         return obj;
     }
 
-    boolean full_match(String src){
-        if (compiledPattern.size() <= 0){
+    boolean full_match(String src) {
+        if (compiledPattern.size() <= 0) {
             return false;
         }
 
         int stateIdx = 1;
-        for(char c : src.toCharArray()){
-            if(stateIdx == 0 || stateIdx >= compiledPattern.size()){
-                return false;
+        for (int idx = 0; idx < src.length();) {
+            if (stateIdx == 0 || stateIdx >= compiledPattern.size()) {
+                break;
             }
-            stateIdx = compiledPattern.get(stateIdx)[c];
+            int currIdx = stateIdx;
+            for (; idx < src.length() && currIdx == stateIdx; ++idx) {
+                char c = src.charAt(idx);
+                stateIdx = compiledPattern.get(stateIdx)[c];
+            }
+            if (currIdx != stateIdx && idx != 1) {
+                --idx;
+            }
+            if (currIdx == stateIdx) {
+                stateIdx++;
+            }
         }
-        return stateIdx == 0? false: true;
+
+        if (stateIdx < compiledPattern.size()) {
+            return false;
+        }
+        return true;
     }
 
-    public static void main(String[] args){
-        Regex r = new Regex("abc");
-        System.out.println(r.full_match("abc"));
+    public static void main(String[] args) {
+        Regex r = new Regex(".b*c");
+        System.out.println(r.full_match("abbbc"));
+        System.out.println(r.full_match("ac"));
+        System.out.println(r.full_match("a"));
+        System.out.println(r.full_match("bbc"));
+        System.out.println(r.full_match("1bc"));
+
+        System.out.println();
+
+        Regex r1 = new Regex(".b+c+");
+        System.out.println(r1.full_match("abbbcc"));
+        System.out.println(r1.full_match("ac"));
+        System.out.println(r1.full_match("bbc"));
+        System.out.println(r1.full_match("1bc"));
+
     }
 }
